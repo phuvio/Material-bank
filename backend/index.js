64e8bdac3af8155c8320f9b71faddde9f0e88845
phuvio <petteri.huvio@gmail.com>
2024-10-19 18:08:21 +0300

    add GET and POST routes for materials
    Add GET route for all material. Add GET route to fetch material by id. Add POST route to insert new material from JSON body. Include error handling for missing materials and query failures.

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,48 @@ const pool = new pg.Pool({
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/api/materials', async (req, res) => {
+  try {
+    const materials = await pool.query(
+      'SELECT id, name, description, visible, is_URL FROM materials WHERE visible=True')
+    res.json(materials.rows)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error retrieving materials' })
+  }
 })
 
-app.get('/api/materials', async (req, res) => {
-  const materials = await (await pool.query('SELECT id, name, description, visible, is_URL FROM materials WHERE visible=True'))
-  res.json(materials.rows)
+app.get('/api/materials/:id', async (req, res) => {
+  const material_id = req.params.id
+  console.log(material_id)
+  try {
+    const result = await pool.query(
+      'SELECT id, name, description, visible, is_URL, URL FROM materials WHERE id=$1',
+      [material_id])
+    console.log(result)
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Material was not found' })
+    }
+    res.json(result.rows)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error retrieving material' })
+  }
+})
+
+app.post('/api/materials', async (req, res) => {
+  const { name, describtion, user_id, visible, is_URL, URL, material } = req.body
+  try {
+    const result = await pool.query(
+      'INSERT INTO materials (name, description, user_id, visible, is_URL, URL, material) \
+        VALUES ($1, $2, $3, $4) RETURNING *',
+      [name, describtion, user_id, visible, is_URL, URL, material])
+    console.log(result)
+    res.json(result.rows)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ error: 'Error saving material' })
+  }
 })
 
 const PORT = 3001

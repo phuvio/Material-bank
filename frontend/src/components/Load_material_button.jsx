9886edead84c69a356a18d2936dbb1b492ea6920
phuvio <petteri.huvio@gmail.com>
2024-10-26 16:53:38 +0300

    add Load_material_button
    Create Load_material_button. Set it open only URL's

---
+++ frontend/src/components/Load_material_button.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const LoadMaterialButton = ({ url }) => {
+    const handleClick = () => {
+        window.open(url, '_blank', 'noopener,noreferrer')
+    }
+
+    return (
+        <button onClick={handleClick} title={url}>
+            Avaa linkki
+        </button>
+    )
+}
+
+export default LoadMaterialButton

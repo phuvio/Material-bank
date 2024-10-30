b76ff5bbc2e8f1aef31463296ddb70529b233f4f
phuvio <petteri.huvio@gmail.com>
2024-10-30 17:53:45 +0200

    add login controller

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 const materialsRouter = require('./controllers/materials')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.use(express.json())
 
 app.use('/api/materials', materialsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 

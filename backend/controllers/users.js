5de0445230c2d70ed65d7b6c0fd5f47596e40048
phuvio <petteri.huvio@gmail.com>
2024-10-30 17:17:48 +0200

    create users controller
    Create users controller and add it to index.js

---
+++ backend/controllers/users.js
@@ -0,0 +1,39 @@
+const router = require('express').Router()
+
+const { User } = require('../models/users')
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll()
+    res.json(users)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error retrieving users' })
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = User.create(req.body)
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    if (user) {
+      res.json(user)
+    } else {
+      res.status(404).json({ error: 'Error saving user' })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Error retrieving user' })
+  }
+})
+
+module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
-const sequelize = require('./config/database')
-const { QueryTypes } = require('sequelize')
-const { Material, User } = require('./models/index')
+
+const materialsRouter = require('./controllers/materials')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -20,89 +20,8 @@ app.use(cors({ origin: allowedOrigins }))
 
 app.use(express.json())
 
-app.get('/api/materials', async (req, res) => {
-  try {
-    const materials = await Material.findAll({
-      attributes: ['id', 'name', 'description', 'visible', 'is_url', 'url'],
-    })
-    res.json(materials)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Error retrieving materials' })
-  }
-})
-
-app.get('/api/materials/:id', async (req, res) => {
-  try {
-    const result = await Material.findOne({
-      attributes: [
-        'id',
-        'name',
-        'description',
-        'user_id',
-        'visible',
-        'is_url',
-        'url',
-        'updated_at',
-      ],
-      where: { id: req.params.id },
-    })
-    console.log(result)
-    if (!result) {
-      return res.status(404).json({ error: 'Material was not found' })
-    }
-    res.json(result)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Error retrieving material' })
-  }
-})
-
-app.get('/api/materials/:id/material', async (req, res) => {
-  try {
-    const result = await Material.findOne({
-      attributes: ['material'],
-      where: { id: req.params.id },
-    })
-    console.log(result)
-    if (!result) {
-      return res.status(404).json({ error: 'Material was not found' })
-    }
-    res.json(result)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Error retrieving material' })
-  }
-})
-
-app.post('/api/materials', async (req, res) => {
-  try {
-    const result = await Material.create(req.body)
-    console.log(result)
-    res.json(result)
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({ error: 'Error saving material' })
-  }
-})
-
-app.post('/api/materials/:id', async (req, res) => {
-  const { name, description, visible, is_URL, URL, material } = req.body
-  const id = req.params.id
-  try {
-    const result = await sequelize.query(
-      'UPDATE materials SET name = $1, description = $2, visible = $3, is_URL = $4, URL = $5, material = $6 \
-        WHERE id = $7 RETURNING *',
-      [name, description, visible, is_URL, URL, material, id],
-      { type: QueryTypes.UPDATE }
-    )
-    console.log(result)
-    res.json(result[0])
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({ error: 'Error saving material' })
-  }
-})
+app.use('/api/materials', materialsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 

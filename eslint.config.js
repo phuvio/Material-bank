9b324f1384d32cd2208ae2cbba312f6438834c1a
phuvio <petteri.huvio@gmail.com>
2024-11-05 18:44:24 +0200

    set linebreak-style to windows in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ module.exports = [
     },
     files: ['backend/**/*.js'],
     rules: {
-      'linebreak-style': 'off',
+      'linebreak-style': ['error', 'windows'],
       'no-console': 'warn',
       //* Avoid Bugs
       'no-undef': 'error',

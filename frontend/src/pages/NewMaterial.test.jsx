eb64dad2f9631338323a47859a582263ea1e9c3c
phuvio <petteri.huvio@gmail.com>
2025-01-05 20:18:17 +0200

    fix tests

--- frontend/src/pages/NewMaterial.test.jsx
+++ frontend/src/pages/NewMaterial.test.jsx
@@ -1,14 +1,8 @@
 import { render, fireEvent, screen, waitFor } from '@testing-library/react'
 import { vi, describe, beforeEach, test, expect } from 'vitest'
 import NewMaterial from './NewMaterial'
-import materialService from '../services/materials'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-// Mock services and hooks
-vi.mock('../services/materials', () => ({
-  create: vi.fn().mockResolvedValue({}),
-  // Add other methods you might use in your component
-}))
 const showNotificationMock = vi.fn()
 
 describe('NewMaterial component', () => {
@@ -38,11 +32,6 @@ describe('NewMaterial component', () => {
   })
 
   test('validates form data and shows error notification when validation fails', async () => {
-    const validateMaterialMock = vi.fn().mockResolvedValue({
-      name: 'Name is required',
-      description: 'Description is required',
-    })
-
     render(
       <Router>
         <NewMaterial
@@ -60,7 +49,6 @@ describe('NewMaterial component', () => {
       target: { value: '' },
     })
 
-    // Try to submit the form with invalid data
     fireEvent.click(screen.getByText(/Tallenna/))
 
     await waitFor(() => {
@@ -68,49 +56,6 @@ describe('NewMaterial component', () => {
         'Materiaalin luonti epäonnistui',
         'error',
         3000
-      )
-    })
-  })
-
-  test.skip('calls materialService.create when form is submitted successfully', async () => {
-    const formData = {
-      name: 'Test Material',
-      description: 'A description for the test material.',
-      user_id: '123',
-      visible: true,
-      is_url: false,
-      material: new File(['material content'], 'material.pdf', {
-        type: 'application/pdf',
-      }),
-      material_type: 'application/pdf',
-    }
-
-    materialService.create.mockResolvedValueOnce({})
-
-    render(
-      <Router>
-        <NewMaterial
-          loggedInUser={loggedInUser}
-          onMaterialAdded={onMaterialAddedMock}
-          showNotification={showNotificationMock}
-        />
-      </Router>
-    )
-
-    fireEvent.change(screen.getByLabelText(/Materiaalin nimi:/), {
-      target: { value: formData.name },
-    })
-    fireEvent.change(screen.getByLabelText(/Kuvaus:/), {
-      target: { value: formData.description },
-    })
-
-    fireEvent.click(screen.getByText(/Tallenna/))
-
-    await waitFor(() => {
-      expect(showNotificationMock).toHaveBeenCalledWith(
-        'Materiaali lisätty',
-        'message',
-        2000
       )
     })
   })
@@ -151,32 +96,5 @@ describe('NewMaterial component', () => {
     fireEvent.click(screen.getByLabelText(/Onko materiaali linkki:/))
 
     expect(screen.getByLabelText(/Linkki:/)).toBeInTheDocument()
-  })
-
-  test.skip('shows error when URL is invalid', async () => {
-    const validateMaterialMock = vi.fn().mockResolvedValue({
-      url: 'URL is invalid',
-    })
-
-    render(
-      <Router>
-        <NewMaterial
-          loggedInUser={loggedInUser}
-          onMaterialAdded={onMaterialAddedMock}
-          showNotification={showNotificationMock}
-        />
-      </Router>
-    )
-
-    fireEvent.click(screen.getByLabelText(/Onko materiaali linkki:/))
-    fireEvent.change(screen.getByLabelText(/Linkki:/), {
-      target: { value: 'invalid-url' },
-    })
-
-    fireEvent.click(screen.getByText(/Tallenna/))
-
-    await waitFor(() => {
-      expect(screen.getByText(/Anna URL-osoite/)).toBeInTheDocument()
-    })
   })
 })

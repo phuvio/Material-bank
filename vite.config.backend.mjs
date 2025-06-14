bc3399a4bdf771f87aac8c8fe403664922d767f4
phuvio <petteri.huvio@gmail.com>
2025-06-14 18:38:21 +0300

    add vitest tests to backend

---
+++ vite.config.backend.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({  
+  test: {  
+    environment: 'node',
+    include: ['backend/**/*.test.{jsx,cjs}'],
+    exclude: ['frontend/**', 'vite.*', 'eslint.*', 'testSetup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './backend/coverage',
+      exclude: ['frontend/**', 'vite.*', 'eslint.*', 'testSetup.js', 'backend/**/*.test.{js,jsx,ts,tsx}']
+    },
+  },
+})

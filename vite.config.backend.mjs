895eccdddd643279c60cd4146d671de348c8abd3
phuvio <petteri.huvio@gmail.com>
2025-06-15 17:33:29 +0300

    update vitest backend config file
    Remove root and add ./backend to reportsDirectory.

--- vite.config.backend.mjs
+++ vite.config.backend.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({  
-  root: './backend',
   test: {  
     environment: 'node',
     include: ['**/*.test.{jsx,cjs}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
-      reportsDirectory: './coverage',
+      reportsDirectory: './backend/coverage',
     },
   },
 })

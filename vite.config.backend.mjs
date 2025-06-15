997a3c932126e7148490dd2395af774bf75952ff
phuvio <petteri.huvio@gmail.com>
2025-06-15 17:39:42 +0300

    reverse vitest config file and change backend lcov-info file to ./coverage

--- vite.config.backend.mjs
+++ vite.config.backend.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({  
+  root: './backend',
   test: {  
     environment: 'node',
     include: ['**/*.test.{jsx,cjs}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
-      reportsDirectory: './backend/coverage',
+      reportsDirectory: './coverage',
     },
   },
 })

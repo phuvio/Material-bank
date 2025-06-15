6cf9178085ab3f6cc732286d1670793ce29c161d
phuvio <petteri.huvio@gmail.com>
2025-06-15 15:48:54 +0300

    fix coverage workflow
    Remove working-directory from workflow. Correct reportsDirectory to ./coverage.

--- vite.config.backend.mjs
+++ vite.config.backend.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
-      reportsDirectory: './backend/coverage',
+      reportsDirectory: './coverage',
     },
   },
 })

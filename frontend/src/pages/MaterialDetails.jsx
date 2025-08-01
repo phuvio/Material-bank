014e04780bc418cd3bee8df4fa6594ab237e253b
phuvio <petteri.huvio@gmail.com>
2025-08-01 14:00:07 +0300

    create materials e2e tests
    Create e2e tests for basic user to add, change and remove materials and change material tags. Fix typos in MaterialDetails page and ColorPicker.

--- frontend/src/pages/MaterialDetails.jsx
+++ frontend/src/pages/MaterialDetails.jsx
@@ -62,7 +62,7 @@ const MaterialDetails = ({ showNotification }) => {
   }
 
   const handleDeleteMaterail = async (id) => {
-    if (window.confirm('Haluatko varmati poistaa tämän materiaalin?')) {
+    if (window.confirm('Haluatko varmasti poistaa tämän materiaalin?')) {
       try {
         await materialService.remove(id)
         showNotification('Materiaali poistettu onnistuneesti', 'message', 2000)

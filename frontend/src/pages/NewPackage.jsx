9c7ffd301d44474d6c2de86fcd9ce4f4c5f8cac3
phuvio <petteri.huvio@gmail.com>
2025-08-20 18:25:32 +0300

    improve new package styling

--- frontend/src/pages/NewPackage.jsx
+++ frontend/src/pages/NewPackage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import packageService from '../services/packages'
 import { validatePackage } from '../utils/packageValidations'
+import materialService from '../services/materials'
 import GoBackButton from '../components/GoBackButton'
 
 const NewPackage = ({ showNotification }) => {
@@ -11,7 +12,27 @@ const NewPackage = ({ showNotification }) => {
     materials: [],
   })
   const [errors, setErrors] = useState({})
+  const [materials, setMaterials] = useState([])
+  const [materialsLoading, setMaterialsLoading] = useState(false)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchMaterials = async () => {
+      try {
+        const initialMaterials = await materialService.getAll()
+        const sortedMaterials = initialMaterials.sort((a, b) =>
+          a.name > b.name ? 1 : -1
+        )
+        setMaterials(sortedMaterials)
+      } catch (error) {
+        console.error('Error fetching materials:', error)
+        showNotification('Virhe haettaessa materiaaleja.', 'error', 3000)
+      } finally {
+        setMaterialsLoading(false)
+      }
+    }
+    fetchMaterials()
+  }, [])
 
   const handleFormChange = (event) => {
     const { name, value } = event.target
@@ -47,36 +68,72 @@ const NewPackage = ({ showNotification }) => {
 
   return (
     <div className="container">
-      <h1>Uusi paketti</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nimi:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleFormChange}
-          />
-          {errors.name && <p className="error">{errors.name}</p>}
-        </div>
-        <div>
-          <label htmlFor="description">Kuvaus:</label>
-          <textarea
-            id="description"
-            name="description"
-            value={formData.description}
-            onChange={handleFormChange}
-          />
-          {errors.description && <p className="error">{errors.description}</p>}
-        </div>
-        <div className="row">
-          <div className="buttongroup">
-            <button type="submit">Tallenna</button>
-            <GoBackButton onGoBack={handleGoBack} />
+      <div className="col-50">
+        <h1>Uusi paketti</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Nimi:  </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleFormChange}
+            />
+            {errors.name && <p className="error">{errors.name}</p>}
           </div>
-        </div>
-      </form>
+          <div>
+            <label htmlFor="description">Kuvaus:</label>
+            <textarea
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleFormChange}
+            />
+            {errors.description && (
+              <p className="error">{errors.description}</p>
+            )}
+          </div>
+          <div className="row">
+            <div className="buttongroup">
+              <button type="submit">Tallenna</button>
+              <GoBackButton onGoBack={handleGoBack} />
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="col-50">
+        <h3>Lisää materiaaleja pakettiin</h3>
+        {materialsLoading ? (
+          <p>Ladataan materiaaleja...</p>
+        ) : (
+          <ul>
+            {materials.map((material) => (
+              <li key={material.id}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={formData.materials.includes(material.id)}
+                    onChange={() => {
+                      setFormData((prevData) => {
+                        const newMaterials = prevData.materials.includes(
+                          material.id
+                        )
+                          ? prevData.materials.filter(
+                              (id) => id !== material.id
+                            )
+                          : [...prevData.materials, material.id]
+                        return { ...prevData, materials: newMaterials }
+                      })
+                    }}
+                  />
+                  {material.name}
+                </label>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   )
 }

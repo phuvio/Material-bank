2e161de50758decaff458fd0745fad14b2d26344
phuvio <petteri.huvio@gmail.com>
2025-08-25 10:58:08 +0300

    add /-sign to regex validation for names and correct LF/CRLF issue

--- frontend/src/utils/validationRegex.jsx
+++ frontend/src/utils/validationRegex.jsx
@@ -1,4 +1,4 @@
-export const regexName = /^[a-zA-ZäöåÄÖÅ0-9\s]+$/
+export const regexName = /^[a-zA-ZäöåÄÖÅ0-9\s/]+$/
 
 export const regexURL =
   /^(https?:\/\/)?([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6}(\/[a-zA-Z0-9_&%$-]*)*$/
